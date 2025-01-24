03eeb1879ff95ea4799a1bb2f48997befd5c3d5a
mahmoud <mahmoudmodi75@gmail.com>
2025-01-24 10:39:41 +0200

    adding features for the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch()
+  const [cartProductCount,setCartProductCount] = useState(0)
+
+  const fetchUserDetails = async()=>{
+      const dataResponse = await fetch(SummaryApi.current_user.url,{
+        method : SummaryApi.current_user.method,
+        credentials : 'include'
+      })
+
+      const dataApi = await dataResponse.json()
+
+      if(dataApi.success){
+        dispatch(setUserDetails(dataApi.data))
+      }
+  }
+
+  const fetchUserAddToCart = async()=>{
+    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url,{
+      method : SummaryApi.addToCartProductCount.method,
+      credentials : 'include'
+    })
+
+    const dataApi = await dataResponse.json()
+
+    setCartProductCount(dataApi?.data?.count)
+  }
+
+  useEffect(()=>{
+    /**user Details */
+    fetchUserDetails()
+    /**user Details cart product */
+    fetchUserAddToCart()
+
+  },[])
   return (
-    <div className="text-red-400">
-      first
-    </div>
+    <>
+      <Context.Provider value={{
+          fetchUserDetails, // user detail fetch 
+          cartProductCount, // current user add to cart product count,
+          fetchUserAddToCart
+      }}>
+        <ToastContainer 
+          position='top-center'
+        />
+        
+        <Header/>
+        <main className='min-h-[calc(100vh-120px)] pt-16'>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </Context.Provider>
+    </>
   );
 }
 
