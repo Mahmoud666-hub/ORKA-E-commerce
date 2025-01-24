ba72b7980b7a1c218517c0197060e234ba5bfa54
mahmoud <mahmoudmodi75@gmail.com>
2025-01-24 10:42:51 +0200

    make modification for app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from 'react';
+import SummaryApi from './common';
+import Context from './context';
+import { useDispatch } from 'react-redux';
+import { setUserDetails } from './store/userSlice';
 
 function App() {
   const dispatch = useDispatch()
